b7b7b538aeec50617523810d82ca651a4f2de009
Erica Nuñez <erikannunez@MacBook-Pro-de-Erica.local>
2016-06-12 21:45:36 -0300

    test

--- TpoProgra/src/uade/tpo/tda/AutorTDA.java
+++ TpoProgra/src/uade/tpo/tda/AutorTDA.java
@@ -13,58 +13,58 @@ public interface AutorTDA {
 
 	/**
 	 * Agrega un autor<br>
-	 * PRE: Estructura inicializada. El autor no debe existir.<br>
+	 * PRE: Estructura inicializada. El autor no debe existir.
 	 * POS: Estructura modificada en un (1) elemento mas
 	 */
 	public void Agregar(String nombreAutor);
 
 	/**
-	 * Devuelve el hijo izquierdo del �rbol de autor<br>
-	 * PRE: Estructura inicializada y no vacia<br>
+	 * Devuelve el hijo izquierdo del arbol de autor
+	 * PRE: Estructura inicializada y no vacia
 	 * POS: -
 	 */
 	public AutorTDA HijoIzquierdo();
 
 	/**
-	 * Devuelve el hijo derecho del arbol de autor<br>
-	 * PRE: Estructura inicializada y no vacia.<br>
+	 * Devuelve el hijo derecho del arbol de autor
+	 * PRE: Estructura inicializada y no vacia.
 	 * POS: -
 	 */
 	public AutorTDA HijoDerecho();
 
 	/**
-	 * Elimina un autor<br>
-	 * PRE: Estructura inicializada y no vacia.<br>
+	 * Elimina un autor
+	 * PRE: Estructura inicializada y no vacia.
 	 * POS: Estructura modificada en un (1) elemento menos.
 	 */
 	public void Eliminar(String nombreAutor);
 
 	/**
-	 * Devuelve una instancia del objeto Autor<br>
-	 * PRE: Estructura inicializada y no vacia<br>
+	 * Devuelve una instancia del objeto Autor
+	 * PRE: Estructura inicializada y no vacia
 	 * POS: -
 	 */
 	public Autor obtenerAutor();
 
 	/**
 	 * Determina si la estructura se encuentra vacia o no. TRUE: estructura
-	 * vacia, FALSE: no vacia.<br>
-	 * PRE: Estructura inicializada<br>
+	 * vacia, FALSE: no vacia.
+	 * PRE: Estructura inicializada
 	 * POS: -
 	 */
 	public boolean AutorVacio();
 
 	/**
-	 * Agrega un libro a un autor.<br>
-	 * PRE: Estructura inicializada y no vacia. El autor debe existir.<br>
+	 * Agrega un libro a un autor.
+	 * PRE: Estructura inicializada y no vacia. El autor debe existir.
 	 * POS: Elemento (autor) modificado en un (1) elemento mas.
 	 */
 	public void AgregarLibro(String nombreAutor, String tituloLibro,
 			float precio);
 
 	/**
-	 * Devuelve una estructura (arbol) con los libros del autor.<br>
-	 * PRE: Estructura inicializada y no vacia. El autor debe existir.<br>
+	 * Devuelve una estructura (arbol) con los libros del autor.
+	 * PRE: Estructura inicializada y no vacia. El autor debe existir.
 	 * POS: -
 	 */
 	public LibrosAutorTDA GetLibros(String nombreAutor);
